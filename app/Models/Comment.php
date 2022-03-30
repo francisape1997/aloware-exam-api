00b0527e81b4adb1c6ac54621c5182cc0f2083c2
Francis <90559251+francisape1997@users.noreply.github.com>
2022-03-30 13:31:01 +0800

    Add comment model

---
+++ app/Models/Comment.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use App\Scopes\CommentScopes;
+
+class Comment extends Model
+{
+    use HasFactory, CommentScopes;
+
+    protected $fillable =
+    [
+        'post_id',
+        'title',
+        'description',
+        'parent_id',
+    ];
+
+    public function parent()
+    {
+        return $this->belongsTo($this, 'parent_id', 'id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany($this, 'parent_id', 'id');
+    }
+}
