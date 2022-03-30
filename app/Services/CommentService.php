ddee4f0d6aacf2d4816b330b780057cb56032675
Francis <90559251+francisape1997@users.noreply.github.com>
2022-03-30 13:30:55 +0800

    Add comment service

---
+++ app/Services/CommentService.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Comment;
+
+class CommentService
+{
+    private $comment;
+
+    public function __construct(Comment $comment)
+    {
+        $this->comment = $comment;
+    }
+
+    public function getComments()
+    {
+        return $this->comment->nestedChildren()->orderBy('created_at', 'DESC')->paginate();
+    }
+
+    public function storeComment($request)
+    {
+        return $this->comment->create(
+            [
+                'post_id'     => $request->post_id,
+                'parent_id'   => $request->comment_id,
+                'title'       => $request->title,
+                'description' => $request->description,
+            ]
+        );
+    }
+}
